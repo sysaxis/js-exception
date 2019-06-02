d6baacedde01f3ef59ff30db45cb2a2f11d3c8c8
sysaxis <sysaxis0@gmail.com>
2019-06-02 22:09:57 +0300

    handle cases where typed error name is non-object

--- test/tests.js
+++ test/tests.js
@@ -25,53 +25,55 @@ class Ex extends Exception {
 
 suite('Exception');
 
+var ex;
+
 test('exception defined with code and codename', function() {
-    const ex = new Ex().TooBusy();
+    ex = new Ex().TooBusy();
 
     assert.strictEqual(ex.code, 1);
     assert.strictEqual(ex.codename, 'TOO_BUZY');
 });
 
 test('exception defined with codename only', function() {
-    const ex = new Ex().TooSimple();
+    ex = new Ex().TooSimple();
 
     assert.strictEqual(ex.codename, 'WAY_TOO_SIMPLE');
 });
 
 test('exception defined with code only', function() {
-    const ex = new Ex().TooEasy();
+    ex = new Ex().TooEasy();
 
     assert.strictEqual(ex.code, 0);
 });
 
 test('new Exception(error: Error)', function() {
     const error = new Error();
-    const ex = new Ex(error);
+    ex = new Ex(error);
 
     assert.ownInclude(ex, error);
 });
 
 test('new Exception(error: String)', function() {
     const message = 'something bad happened';
-    const ex = new Ex(message);
+    ex = new Ex(message);
 
     assert.ownInclude(ex, {message});
 });
 
 test('new Exception(error: Object)', function() {
-    const ex = new Ex({a: 1});
+    ex = new Ex({a: 1});
 
     assert.hasAllKeys(ex, ['message', 'stack']);
 });
 
 test ('new Exception(error: undefined)', function() {
-    const ex = new Ex();
+    ex = new Ex();
 
     assert.hasAllKeys(ex, ['message', 'stack']);
 });
 
 test('creating with typed error name', function() {
-    const ex = new Ex().TooBusy();
+    ex = new Ex().TooBusy();
 
     assert.ownInclude(ex, {
         code: 1,
@@ -80,7 +82,7 @@ test('creating with typed error name', function() {
 });
 
 test('adding custom information', function() {
-    const ex = new Ex().TooBusy({
+    ex = new Ex().TooBusy({
         waiting: true
     });
 
@@ -90,7 +92,7 @@ test('adding custom information', function() {
 });
 
 test('#toString()', function() {
-    const ex = new Ex();
+    ex = new Ex();
 
     assert.equal(ex.message || ex.codename, ex.toString());
 });
@@ -104,19 +106,38 @@ test('#toObject()', function() {
         speed: 2,
         accelerating: false
     };
-    const ex = new Ex(error).TooSlow(params);
+    ex = new Ex(error).TooSlow(params);
 
-    const obj = ex.toObject();
+    var obj = ex.toObject();
     
-    assert.deepOwnInclude(obj, Object.assign(error, params, {
+    assert.deepOwnInclude(obj, Object.assign({}, error, params, {
         code: 2,
         codename: 'TOO_SLOW'
+    }));
+
+    ex = new Ex(error).TooSlow(800);
+    obj = ex.toObject();
+
+    assert.deepOwnInclude(obj, Object.assign({}, error, {
+        details: 800
+    },{
+        code: 2,
+        codename: 'TOO_SLOW'
+    }));
+
+    ex = new Ex().TooSlow('800');
+    obj = ex.toObject();
+
+    assert.deepOwnInclude(obj, Object.assign({}, error, {
+        code: 2,
+        codename: 'TOO_SLOW',
+        message: '800'
     }));
 });
 
 test('#toJSON()', function() {
 
-    const ex = new Ex();
+    ex = new Ex();
 
     var json = ex.toJSON(true);
     var expected = `{"stack":"${ex.stack.replace(/\\/g, '\\\\').replace(/\r?\n/g, '\\n')}","message":""}`;
@@ -128,7 +149,7 @@ test('#toJSON()', function() {
 });
 
 test('#deserialize(string: String)', function() {
-    var ex = new Ex();
+    ex = new Ex();
 
     var obj = ex.toJSON(true);
 
@@ -139,7 +160,7 @@ test('#deserialize(string: String)', function() {
 });
 
 test('#isException(error: any)', function() {
-    var ex = new Ex();
+    ex = new Ex();
 
     assert.isTrue(Ex.isException(ex));
     assert.isNotTrue(Ex.isException(null));
